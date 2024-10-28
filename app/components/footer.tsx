658c13dd1a6d29e7984d0714c9fd88434242a9c5
marcho <madergilev@edu.hse.ru>
2024-10-29 00:45:54 +0300

    init

---
+++ app/components/footer.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+
+export function Footer() {
+  return (
+    <footer className="w-full bg-black">
+      <div className="w-full bg-gray-500 flex-1">
+        <p>© {new Date().getFullYear()} DAO.BUILD. All rights reserved.</p>
+        <nav>
+          <ul className="">
+            <li><Link href="/about">About Us</Link></li>
+            <li><Link href="/privacy">Privacy Policy</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </div>
+      <div className="flex-1 flex justify-end">
+          sdassa
+      </div>
+    </footer>
+  );
+}
